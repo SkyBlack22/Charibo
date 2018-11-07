9b84c8b834824e1d4d8fe4669a42e5ae91e1c559
Frozox <34484876+Frozox@users.noreply.github.com>
2018-11-07 21:55:30 +0100

    test

---
+++ Charibo_projet_jeu/Source/Code/CorePlugin/YourCustomComponentType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Duality;
+using Duality.Components.Physics;
+using OpenTK;
+using OpenTK.Input;
+
+
+namespace Charibo
+{
+    [RequiredComponent(typeof(RigidBody))]
+    public class controle : Component
+	{
+		
+	}
+}
