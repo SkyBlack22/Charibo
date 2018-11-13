18523648abcccb8273309b4e34daaed95bac7fbf
Frozox <34484876+Frozox@users.noreply.github.com>
2018-11-13 22:29:58 +0100

    update(2)

---
+++ Unity/Charibo/Assets/camera/cam_angle.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cam_angle : MonoBehaviour
+{
+    public GameObject voiture;
+
+    private Vector3 offset;
+
+    void Start ()
+    {
+        offset = transform.position - voiture.transform.position;
+    }
+
+	private void LateUpdate ()
+    {
+        transform.position = voiture.transform.position + offset;
+    }
+}
